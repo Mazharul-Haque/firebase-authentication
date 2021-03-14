f33cf8522d303c09dc90349a3272905f342fa958
Mazharul-Haque-Sagor <mshagor009@gmail.com>
2021-03-14 12:35:06 +0600

    firebase authentication

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAgC3frzuSVEbv-uKnTwbYzktS4xZOvErA",
+  authDomain: "ema-jhon-react-app.firebaseapp.com",
+  projectId: "ema-jhon-react-app",
+  storageBucket: "ema-jhon-react-app.appspot.com",
+  messagingSenderId: "1034252743978",
+  appId: "1:1034252743978:web:04670e50a7f2896a621a53"
+};
+
+export default firebaseConfig;
